4384a061fac0c1efd8d8871bfd7b1b94ebc70d25
Deniss <den.b@itservicesgroup.co.uk>
2016-03-28 19:52:27 +0100

    Add #callHeads and #callTails , work in progress, no tests yet

--- CoinToss/ViewController.h
+++ CoinToss/ViewController.h
@@ -8,7 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    UILabel *status;
+    UILabel *result;
+
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *status;
+@property (nonatomic, retain) IBOutlet UILabel *result;
+
+-   (IBAction)callHeads;
+-   (IBAction)callTails;
 
 
 @end
--- CoinToss/ViewController.m
+++ CoinToss/ViewController.m
@@ -13,15 +13,54 @@
 @end
 
 @implementation ViewController
+@synthesize status, result;
 
+- (void) simulateCoinToss: (BOOL) userCalledHeads {
+    BOOL coinLandedOnHeads = (arc4random() %2) == 0;
+    result.text = coinLandedOnHeads ? @"Heads" : @"Tails";
+    if (coinLandedOnHeads == userCalledHeads)
+        status.text = @"Correct!";
+    else
+        status.text = @"Wrong!";
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    rotation.fromValue = [NSNumber numberWithFloat:0.0f];
+    rotation.toValue = [NSNumber numberWithFloat: 720 * M_PI/ 180.0f];
+    rotation.duration = 2.0f;
+    [status.layer addAnimation: rotation forKey:@"rotate"];
+    CABasicAnimation *fade = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fade.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    fade.fromValue = [NSNumber numberWithFloat:0.0f];
+    fade.toValue = [NSNumber numberWithFloat:1.0f];
+    fade.duration = 3.5f;
+    [status.layer addAnimation:fade forKey:@"fade"];
+}
+-(IBAction)callHeads{
+    [self simulateCoinToss:YES];
+}
+-(IBAction)callTails{
+    [self simulateCoinToss:NO];
+}
+
+- (IBAction)callTails:(id)sender forEvent:(UIEvent *)event {
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (void)viewDidUnLoad {
+    self.status = nil;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//-(void) dealloc {
+  //  [status release];
+  // [result release];
+  //[super dealloc];
+// }
 
 @end
